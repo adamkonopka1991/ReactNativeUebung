d763d02bc32a8faff58c6d3d40b2864cceb93032
Adam <adam.konopka9@outlook.de>
2019-04-13 09:46:41 +0200

    Right after installing image picker

--- rnapp/src/screens/PlaceDetail/PlaceDetail.js
+++ rnapp/src/screens/PlaceDetail/PlaceDetail.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {View, Image, Text, Button, StyleSheet, TouchableOpacity, Platform, Dimensions} from 'react-native';
 import {connect} from 'react-redux';
+import MapView from "react-native-maps";
+
 import Icon from 'react-native-vector-icons/Ionicons';
 import {deletePlace} from '../../store/actions/index';
 
@@ -41,9 +43,23 @@ class PlaceDetail extends Component {
                     this.state.viewMode==="portrait" 
                     ? styles.portraitContainer 
                     : styles.landscapeContainer]}>
-                <View style={styles.subContainer}>
-                    <Image source={this.props.selectedPlace.image} style={styles.placeImage} />
+                <View style={styles.placeDetailContainer}>
+                    <View style={styles.subContainer}>
+                        <Image source={this.props.selectedPlace.image} style={styles.placeImage} />
+                    </View>
+                    <View style={styles.subContainer}>
+                        <MapView
+                            initialRegion={{
+                                ...this.props.selectedPlace.location,
+                                latitudeDelta: 0.0122,
+                                longitudeDelta: Dimensions.get("window").width/Dimensions.get("window").height * 0.0122
+                            }}
+                            style={styles.map}>
+                            <MapView.Marker coordinate={this.props.selectedPlace.location} />
+                        </MapView>
+                    </View>
                 </View>
+                
                 <View style={styles.subContainer}>
                     <View>
                         <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
@@ -76,17 +92,23 @@ const styles= StyleSheet.create({
     landscapeContainer: {
         flexDirection: "row"
     },
+    placeDetailContainer: {
+        flex: 2
+    },
     subContainer: {
         flex: 1
     },
     placeImage: {
-        height: 200,
+        height: "100%",
         width: "100%"
     },
     placeName: {
         fontWeight: "bold",
         textAlign: "center",
         fontSize: 28
+    },
+    map:{
+        ...StyleSheet.absoluteFillObject //so the object fills the surrounding container
     },
     deleteButton: {
         alignItems: "center"
